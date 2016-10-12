0389ab2914648a9ae4b6be9073c52c44073f3665
Jess Lee <jesslee1315@gmail.com>
2016-10-12 10:23:09 -0600

    splash page time and logo changes

--- js/main.js
+++ js/main.js
@@ -11,13 +11,13 @@
     //function logo rotator on click
     function loader(){
       tl
-      .to(pre_loader_logo, 4, {rotation:360, ease:Power0.easeNone})
+      .to(pre_loader_logo, 3, {rotation:360, ease:Power0.easeNone})
       .to([title, pre_loader_logo], 1, {opacity:0 , ease:Power1.easeInOut})
       .add('red')
       .add('blue')
 
-      .to(blue, 1.5, {x: 500, ease:Power1.easeInOut, opacity:0}, 'blue')
-      .to(red, 1.5, {x: -500, ease:Power1.easeInOut, opacity:0}, 'red')
+      .to(blue, 1.2, {x: 500, ease:Power1.easeInOut, opacity:0}, 'blue')
+      .to(red, 1.2, {x: -500, ease:Power1.easeInOut, opacity:0}, 'red')
 
       tl.pause();
       $('.title, #preLoaderLogo').click(function(){
